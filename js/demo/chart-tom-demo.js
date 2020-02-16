fc9fb6a7fb7de42e571774c55fa63f0f31bd7388
Tom Yan <tomgg200@hotmail.com>
2020-02-16 00:00:13 -0500

    welcome

--- js/demo/chart-tom-demo.js
+++ js/demo/chart-tom-demo.js
@@ -27,12 +27,12 @@ function number_format(number, decimals, dec_point, thousands_sep) {
   return s.join(dec);
 }
 
-// Area Chart Example
-var ctx = document.getElementById("myAreaChart");
-var myLineChart = new Chart(ctx, {
+// Area Chart #2
+var ctx2 = document.getElementById("tomYan");
+var newLineChart = new Chart(ctx2, {
   type: 'line',
   data: {
-    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+    labels: ["Sep", "Oct", "Nov", "Dec"],
     datasets: [{
       label: "Earnings",
       lineTension: 0.3,
@@ -46,7 +46,7 @@ var myLineChart = new Chart(ctx, {
       pointHoverBorderColor: "rgba(78, 115, 223, 1)",
       pointHitRadius: 10,
       pointBorderWidth: 2,
-      data: [0, 10000, 5000, 15000, 10000, 20000, 15000, 25000, 20000, 30000, 25000, 40000],
+      data: [10000, 5000, 15000, 10000],
     }],
   },
   options: {
