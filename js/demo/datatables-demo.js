cb26737078c161f68546fa8948245b11674c558c
Tom Yan <tomgg200@hotmail.com>
2020-02-16 04:28:45 -0500

    final commit

--- js/demo/datatables-demo.js
+++ js/demo/datatables-demo.js
@@ -2,3 +2,27 @@
 $(document).ready(function() {
   $('#dataTable').DataTable();
 });
+
+// Set the configuration for your app
+// TODO: Replace with your project's config object
+var config = {
+  apiKey: "apiKey",
+  authDomain: "projectId.firebaseapp.com",
+  databaseURL: "https://databaseName.firebaseio.com",
+  storageBucket: "bucket.appspot.com"
+};
+firebase.initializeApp(config);
+// Get a reference to the database service
+var database = firebase.database();
+console.log(firebase);
+
+var ref = database.ref('AxData');
+ref.on('value', gotData, errData);
+
+function gotData(data) {
+  console.log(data);
+}
+function errDate(error) {
+  console.log('Error!!');
+  console.log(error);
+}
